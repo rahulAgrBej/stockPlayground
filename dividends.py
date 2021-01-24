4c7ea33735ce6e4e4379ff5130466b54f1210af3
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-01-24 18:34:20 -0500

    reading in dividend aristocrats

--- dividends.py
+++ dividends.py
@@ -1,7 +1,29 @@
 import yfinance as yf
 import pprint
 
+def readAristocrats(filePath):
+
+    aristocrats = []
+
+    f = open(filePath, 'r', encoding='utf-8-sig')
+    rows = f.readlines()
+    f.close()
+
+    for row in rows:
+        row = row.rstrip('\n')
+        cells = row.split(',')
+        aristocrats.append((cells[0], cells[1]))
+    
+    return aristocrats
+
+aristocrats = readAristocrats('dividend_aristocrats.csv')
+pp = pprint.PrettyPrinter(indent=3)
+pp.pprint(aristocrats)
+
+
+"""
 abbv = yf.Ticker("ABBV")
 
-pp = pprint.PrettyPrinter(indent=3)
+
 pp.pprint(abbv.info)
+"""
